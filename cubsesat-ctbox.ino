b3990b08719385745d3cb1344bc9269b3bb2258e
Saurabh Wanivadekar <aw14saurabh@hotmail.com>
2022-04-06 15:43:40 -0400

    Added inputs and outputs

--- cubsesat-ctbox.ino
+++ cubsesat-ctbox.ino
@@ -3,6 +3,13 @@
 #include <RF24.h>
 
 #define CTBOX 1
+#define ROLL_Y 2
+#define PITCH_Y 3
+#define YAW_Y 4
+#define LASER 5
+#define SATOP 6
+#define LED_D 7
+#define LED_P 8
 
 #define NRF_CE 14
 #define NRF_CS 15
@@ -20,6 +27,9 @@ union uAngleBytes
 
 RF24 radio(NRF_CE, NRF_CS);
 angRPYData_t satAngles;
+uint32_t roll;
+uint32_t pitch;
+uint32_t yaw;
 
 messageData_t messageOut;
 uint8_t rxAddr[][6] = {"00001", "00002"};
@@ -39,11 +49,24 @@ void setup()
 {
     Wire.begin(CTBOX);
     Wire.onRequest(requestEvent);
+
+    pinMode(NRF_CE, OUTPUT);
+    pinMode(NRF_CS, OUTPUT);
     radio.begin();
     radio.setRetries(15, 15);
     radio.openWritingPipe(rxAddr[0]);
     radio.openReadingPipe(0, rxAddr[1]);
     radio.stopListening();
+
+    pinMode(ROLL_Y,  INPUT);
+    pinMode(PITCH_Y, INPUT);
+    pinMode(YAW_Y,   INPUT);
+    pinMode(LASER,   INPUT);
+    pinMode(SATOP,   INPUT);
+    pinMode(LED_D,  OUTPUT);
+    pinMode(LED_P,  OUTPUT);
+
+    satAngles = {0.0, 0.0, 0.0};
 }
 
 void loop()
@@ -52,6 +75,19 @@ void loop()
     txDt = currentMillis - prevTxMillis;
 
     /* Get all inputs from physical devices here and store it in messageOut*/
+    messageOut.laserEnable = digitalRead(LASER);
+    messageOut.opMode = digitalRead(SATOP);
+
+    roll = analogRead(ROLL_Y);
+    pitch = analogRead(PITCH_Y);
+    yaw = analogRead(YAW_Y);
+    if (roll > 1000) satAngles.x += 0.2;
+    else if (roll < 100) satAngles.x -= 0.2;
+    if (pitch > 1000) satAngles.y += 0.2;
+    else if (pitch < 100) satAngles.y -= 0.2;
+    if (yaw > 1000) satAngles.z += 0.2;
+    else if (yaw < 100) satAngles.z -= 0.2;
+    messageOut.targetAngles = satAngles;
 
     radio.stopListening();
     if (txDt >= MIN_TX_TIME)
